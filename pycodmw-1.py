6be2be4b7b8da62ad37a0db4dd7fd0a6e8a26cbc
Rio Weber <riodweber@gmail.com>
2020-04-18 14:55:40 -0400

    SEND INPUT WORKING; Convert captured input;

--- pycodmw-1.py
+++ pycodmw-1.py
@@ -1,20 +1,45 @@
+import time
+
+# Installed
 import numpy as np
 from PIL import ImageGrab
 import cv2
-import time
+
+# Project
+from directkeys import PressKey, W, A, S, D
 
 
-last_time = time.time()
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
 
-while(True):
-    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-    # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8').reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
+def main():
+    
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
 
-    print('Loop took {} seconds'.format(time.time() - last_time))
     last_time = time.time()
 
-    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    while True:
+        PressKey(W)
 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        # 800x600 windowed mode for Call of Duty Modern Warzone, at the top left position of main screen.
+        # 40 px accounts for title bar. 
+        screen =  np.array(ImageGrab.grab(bbox=(0, 40, 800, 530)))
+        print('Frame took {} seconds'.format(time.time()-last_time))
+
+        last_time = time.time()
+        new_screen = process_img(screen)
+        # cv2.imshow('window', new_screen)
+        cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+main()
